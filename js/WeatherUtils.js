5ba8f6e9f003da7d721d89dbdd064421f61fbc12
Pradeep B <praseodeveloper@gmail.com>
2021-02-25 00:53:19 +0100

    Second version New style for 320pt x 240pt cards

--- js/WeatherUtils.js
+++ js/WeatherUtils.js
@@ -6,10 +6,14 @@ function getCityName(cityName){
     const cityNames = [
         { old : "REG.-BEZ. Karlsruhe", "new" :  "Karlsruhe"},
         { old: "Gautam Buddha Nagar", "new" : "NOIDA"},
+        { old: "DĀDRI", "new" : "NOIDA"},
+        { old: "ZAIDPUR", "new" : "Lucknow"},
+        { old: "URAN", "new" : "Mumbai"},
         { old: "Thiruvananthapuram", "new" : "Trivandrum"},
         { old: "Bengaluru", "new" : "Bangalore"},
         { old: "REG.-BEZ. STUTTGART", "new" : "Stuttgart"},
         { old: "", "new" : "Stockholm"},
+        { old: "Wellesbourne Mountford", "new" : "Stratford"}
     ];
     let renamedCityEntry = cityNames.filter(entry => entry.old.toLowerCase() === cityName.toLowerCase());
     return renamedCityEntry.length ? renamedCityEntry[0]["new"] : cityName;
@@ -36,6 +40,13 @@ function loadWeather(postalCode, country){
           success: success,
           dataType: "json"
     });
+
+        $.ajax({
+              url: "/weatherdataforecast",
+              data: "postalCode=" + postalCode + "&country=" + country,
+              success: successForecast,
+              dataType: "json"
+        });
 }
 
 //var methods = {
@@ -54,35 +65,139 @@ function loadWeather(postalCode, country){
 
 function success(weatherData){
     if (weatherData && weatherData.data && weatherData.data.length) {
-        document.getElementById("city_name").innerHTML = getCityName(weatherData.city_name); // + ", " + weatherData.country_code;
+        document.getElementById("city_name").innerHTML = getCityName(weatherData.data[0].city_name); // + ", " + weatherData.country_code;
         document.getElementById("temp").innerHTML = weatherData.data[0].temp.toFixed(1) +"&deg;C";
-        document.getElementById("max_temp").innerHTML = weatherData.data[0].max_temp.toFixed(1) +"&deg;";
-        document.getElementById("min_temp").innerHTML = weatherData.data[0].min_temp.toFixed(1) +"&deg;";
         document.getElementById("wind_spd").innerHTML = Math.round(weatherData.data[0].wind_spd * 3.6) +" km/h";
-        document.getElementById("ob_time").innerHTML = getLocalTime(weatherData.timezone);
+        document.getElementById("wind_dir").innerHTML = weatherData.data[0].wind_cdir;
         document.getElementById("weatherDescription").innerHTML = weatherData.data[0].weather.description;
-        document.getElementById("weatherIcon").src =  weatherIconUrl + weatherData.data[0].weather.icon + ".png";
+        document.getElementById("weatherIcon").src =  "../icon/" + weatherData.data[0].weather.icon + ".png";
+        document.getElementById("uv").innerHTML = weatherData.data[0].uv;
 
-        for(var index=1;index<7;index++) {
+        //icons
+        document.getElementById("windIcon").className = "windDirection wi wi-wind wi-towards-"+ weatherData.data[0].wind_cdir.toLowerCase();
+
+        setBackColor(weatherData.data[0].pod);
+        updateTime(weatherData.data[0].timezone);
+    }
+}
+
+function successForecast(weatherData){
+    if (weatherData && weatherData.data && weatherData.data.length) {
+          document.getElementById("max_temp").innerHTML = weatherData.data[0].max_temp.toFixed(0);
+          document.getElementById("min_temp").innerHTML = weatherData.data[0].min_temp.toFixed(0);
+          document.getElementById("moon_phase").className = "wi " + getMoonPhase(weatherData.data[0].moon_phase_lunation);
+          document.getElementById("sunrise").innerHTML = getRegionalTime(weatherData.data[0].sunrise_ts, weatherData.timezone);
+          document.getElementById("sunset").innerHTML = getRegionalTime(weatherData.data[0].sunset_ts, weatherData.timezone);
+          document.getElementById("precip").innerHTML = weatherData.data[0].pop + "%";
+
+        //for next 3 days
+        for(var index=1;index<4;index++) {
             document.getElementById("day" + index).innerHTML = getWeekday(weatherData.data[index].ts);
-            document.getElementById("temp" + index).innerHTML = weatherData.data[index].temp+"&deg;";
+            document.getElementById("mintemp" + index).innerHTML = weatherData.data[index].min_temp.toFixed(0);
+            document.getElementById("maxtemp" + index).innerHTML = weatherData.data[index].max_temp.toFixed(0);
+            document.getElementById("weatherForecastIcon" + index).src = "../icon/" + weatherData.data[index].weather.icon + ".png";
         }
+
+    }
+}
+
+function updateTime(timezone) {
+  document.getElementById("ob_time").innerHTML = getLocalTime(timezone);
+  var t = setTimeout(function(){ updateTime(timezone) }, 1000); /* setting timer */
+}
+
+function getMoonPhase(lunation){
+    let index = Math.round(lunation * 28);
+    let phaseMap =[
+        "wi-moon-new",
+        "wi-moon-waxing-crescent-1",
+        "wi-moon-waxing-crescent-2",
+        "wi-moon-waxing-crescent-3",
+        "wi-moon-waxing-crescent-4",
+        "wi-moon-waxing-crescent-5",
+        "wi-moon-waxing-crescent-6",
+        "wi-moon-first-quarter",
+        "wi-moon-waxing-gibbous-1",
+        "wi-moon-waxing-gibbous-2",
+        "wi-moon-waxing-gibbous-3",
+        "wi-moon-waxing-gibbous-4",
+        "wi-moon-waxing-gibbous-5",
+        "wi-moon-waxing-gibbous-6",
+        "wi-moon-full",
+        "wi-moon-waning-gibbous-1",
+        "wi-moon-waning-gibbous-2",
+        "wi-moon-waning-gibbous-3",
+        "wi-moon-waning-gibbous-4",
+        "wi-moon-waning-gibbous-5",
+        "wi-moon-waning-gibbous-6",
+        "wi-moon-third-quarter",
+        "wi-moon-waning-crescent-1",
+        "wi-moon-waning-crescent-2",
+        "wi-moon-waning-crescent-3",
+        "wi-moon-waning-crescent-4",
+        "wi-moon-waning-crescent-5",
+        "wi-moon-waning-crescent-6"
+    ];
+
+    return phaseMap[index];
+}
+
+function setBackColor(pod){
+    if (pod === 'd')
+    {
+        document.getElementById("tile").style['background'] = '#000000';//"#7abcff";
+    }
+    else
+    {
+        document.getElementById("tile").style['background'] = '#000000';//''#383938';
     }
 }
 
 function getLocalTime(timeZone){
-    let options = {
-        timeZone: timeZone,
-        weekday: 'short',
-        year: 'numeric',
-        month: 'numeric',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric'
-      },
-      formatter = new Intl.DateTimeFormat([], options);
+//    let options = {
+//        timeZone: timeZone,
+//        weekday: 'short',
+//        year: 'numeric',
+//        month: 'numeric',
+//        day: 'numeric',
+//        hour: 'numeric',
+//        minute: 'numeric'
+//      },
+    var options = { timeZone: timeZone, weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' ,
+    hour: 'numeric', minute: 'numeric', second: 'numeric'};
+    var formatter = new Intl.DateTimeFormat([], options);
 
     return formatter.format(new Date());
+}
+
+function getRegionalTime(unixTimestamp, timeZone){
+    //let unix_timestamp = 1549312452
+    // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    var date = new Date(unixTimestamp * 1000);
+//    var year = date.getYear();
+//    var month = date.getMonth();
+//    var day = date.getDay();
+//    // Hours part from the timestamp
+//    var hours = date.getHours();
+//    // Minutes part from the timestamp
+//    var minutes = "0" + date.getMinutes();
+//    // Seconds part from the timestamp
+//    var seconds = "0" + date.getSeconds();
+
+    // Will display time in 10:30:23 format
+    //var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    // let utcDate = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+
+   // console.log('Given IST datetime: ' + date);
+
+    let intlDateObj = new Intl.DateTimeFormat('en-US', {
+        hour: 'numeric', minute: 'numeric',
+        timeZone: timeZone
+    });
+    let formattedTime = intlDateObj.format(date);
+
+    return formattedTime;
 }
 
 function getWeekday(unixTimestamp){
@@ -93,25 +208,3 @@ function getWeekday(unixTimestamp){
      // Weekday
     return days_arr[date.getDay()];
 }
-
-//function convertStampDate(unixTimestamp){
-//    // Months array
-//    var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-//    // Days array
-//    var days_arr = ['Sun', 'Mon','Tue', 'Wed','Thu', 'Fri','Sat'];
-//    // Convert timestamp to milliseconds
-//    var date = new Date(unixTimestamp*1000);
-//    // Year
-//    var year = date.getFullYear();
-//    // Month
-//    var month = months_arr[date.getMonth()];
-//    // Day
-//    var day = date.getDate();
-//    // Weekday
-//    var weekday = days_arr[date.getDay()];
-//    // Display date time in MM-dd-yyyy h:m:s format
-//    var fullDate = weekday + ', ' + month+' '+day+'-'+year; //+''+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-//    return fullDate;
-//}
-
-//module.exports = methods;
